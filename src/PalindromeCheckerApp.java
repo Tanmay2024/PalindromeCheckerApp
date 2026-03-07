2882081c897b4d78e92a2bb9761c30d809119e60
Tanmay2024 <tanmayguruvugari2007@gmail.com>
2026-03-07 22:14:27 +0530

    UC13:Performance comparison

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,26 +1,23 @@
-interface PalindromeStrategy {
-    boolean check(String input);
-}
-class StackStrategy implements PalindromeStrategy {
-    public boolean check(String input) {
-        java.util.Stack<Character> stack = new java.util.Stack<>();
-        for (char c : input.toCharArray()) {
-            stack.push(c);
-        }
-        for (char c : input.toCharArray()) {
-            if (c != stack.pop()) {
-                return false;
-            }
-        }
-        return true;
-    }
-}
 public class PalindromeCheckerApp {
     public static void main(String[] args){
         String input = "Level";
-        PalindromeStrategy strategy = new StackStrategy();
-        boolean result = strategy.check(input.toLowerCase());
+        long startTime = System.nanoTime();
+        boolean isPalindrome = true;
+        int start = 0;
+        int end = input.length() - 1;
+        while (start < end) {
+            if (Character.toLowerCase(input.charAt(start)) !=
+                    Character.toLowerCase(input.charAt(end))) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+        long endTime = System.nanoTime();
+        long executionTime = endTime - startTime;
         System.out.println("Input : " + input);
-        System.out.println("Is Palindrome? : " + result);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+        System.out.println("Execution Time : " + executionTime + " ns");
     }
 }
