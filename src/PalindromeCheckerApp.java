e58604c6fa56ae4172443fdc398e6e32bb07ceaa
Tanmay2024 <tanmayguruvugari2007@gmail.com>
2026-03-07 22:08:05 +0530

    UC12:strategy pattern palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,22 +1,25 @@
-class PalindromeChecker {
-    public boolean checkPalindrome(String input) {
-        int start = 0;
-        int end = input.length() - 1;
-        while (start < end) {
-            if (input.charAt(start) != input.charAt(end)) {
+interface PalindromeStrategy {
+    boolean check(String input);
+}
+class StackStrategy implements PalindromeStrategy {
+    public boolean check(String input) {
+        java.util.Stack<Character> stack = new java.util.Stack<>();
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop()) {
                 return false;
             }
-            start++;
-            end--;
         }
         return true;
     }
 }
 public class PalindromeCheckerApp {
     public static void main(String[] args){
-        String input = "racecar";
-        PalindromeChecker checker = new PalindromeChecker();
-        boolean result = checker.checkPalindrome(input);
+        String input = "Level";
+        PalindromeStrategy strategy = new StackStrategy();
+        boolean result = strategy.check(input.toLowerCase());
         System.out.println("Input : " + input);
         System.out.println("Is Palindrome? : " + result);
     }
